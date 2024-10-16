73e05a16b696ab26a8c6e5dd40274b1a6f0125ec
Anastasiia <73668938+anastasiia-melnyk@users.noreply.github.com>
2024-10-16 12:10:37 +0300

    Add files via upload

---
+++ Python/courses/learn-python-3/projects/lesson-1-hello-world/block-letters/initials.py
@@ -0,0 +1,14 @@
+# This section prints the initials 'A' and 'M' in block letters in accordance with tasks 1, 3, 4
+
+print("    A        M           M")
+print("  A   A      M  M     M  M")
+print(" A     A     M   M   M   M")
+print("A A A A A    M     M     M")
+print("A       A    M           M")
+print("A       A    M           M")
+print("A       A    M           M")
+
+# This section contains comments in accordance with task 2
+
+# First and last name: Anastasiia Melnyk.
+# A fun fact about me: I try to be responsible for everything I do, but for some reason, I always forget to water the flowers. That's why even a cactus can die.
